df43fb98c014058d0a498274f0173f40fb8b58ce
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2023-08-11 15:15:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oyjJH6g8JmecTrniORrQT7cjP6t0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oyjJH6g8JmecTrniORrQT7cjP6t0',
+      useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-24',
       festivals: [
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oyjJH6g8JmecTrniORrQT7cjP6t0',
     }
   ],
 
