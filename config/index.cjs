800e1695a5ed00f4898015e7748d2e772cd423ec
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2023-08-11 17:14:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'Kayce Baby',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyjJH6g8JmecTrniORrQT7cjP6t0',
+      id: 'oyjJH6oVXuFoqo0qROBGzH33alh8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Ne6GC3l-VREsPG2HNioOckoRfFL6KaogRWFY3iqeduU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Ne6GC3l-VREsPG2HNioOckoRfFL6KaogRWFY3iqeduU',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oyjJH6g8JmecTrniORrQT7cjP6t0',
     }
   ],
 
