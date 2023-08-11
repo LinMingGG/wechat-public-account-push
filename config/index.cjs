997fab1346d49dc41804896de4aac56167200807
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2023-08-11 15:26:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,7 +13,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '8cc01c51355180f3303f477a69b6521a',
 
-  PROVINCE: '重庆',
+  PROVINCE: '重庆市',
   CITY: '重庆',
 
   USERS: [
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyjJH6g8JmecTrniORrQT7cjP6t0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'bAXhXcUbH3GBfKvVzCPUttxhJ600JUB6DQMQyH2BlYY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-24',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'bAXhXcUbH3GBfKvVzCPUttxhJ600JUB6DQMQyH2BlYY',
 
   CALLBACK_USERS: [
     {
